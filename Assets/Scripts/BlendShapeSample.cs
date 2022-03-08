b5b2c18301b9d4e7fd7bd0d4e092bc66f2d63fa8
Jing Tan <jtan1993@gmail.com>
2022-03-08 14:29:24 +0000

    set up animation clips example

--- Assets/Scripts/BlendShapeSample.cs
+++ Assets/Scripts/BlendShapeSample.cs
@@ -6,8 +6,9 @@ public class BlendShapeSample : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer CubeSkinnedMeshRenderer;
     private Mesh _mesh;
-    [SerializeField] private KeyCode blendShapeKey0 = KeyCode.A;
-    [SerializeField] private KeyCode blendShapeKey1 = KeyCode.S;
+    [SerializeField] private KeyCode blendShape0TriggerKey = KeyCode.A;
+    [SerializeField] private KeyCode blendShape1TriggerKey = KeyCode.S;
+    [SerializeField] private KeyCode blendShapeOriginalTriggerKey = KeyCode.D;
     
     void Start()
     {
@@ -29,15 +30,31 @@ public class BlendShapeSample : MonoBehaviour
             Debug.Log(blendShapeName[i] + ": " + blendShapeIndex[i]);
         }
 
-        if (Input.GetKeyDown(blendShapeKey0)) {
+        if (Input.GetKeyDown(blendShape0TriggerKey)) {
             CubeSkinnedMeshRenderer.SetBlendShapeWeight(0, 100);
             if (CubeSkinnedMeshRenderer.GetBlendShapeWeight(1) != 0) {
                 CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 0);
             }
         }
 
-        if (Input.GetKeyDown(blendShapeKey1)) {
+        if (Input.GetKeyDown(blendShape1TriggerKey)) {
             CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 100);
+        }
+
+        ResetBlendShapeToOriginalOnTrigger();
+    }
+    
+    // TODO: Create function to reset blendshape
+    void ResetBlendShapeToOriginalOnTrigger()
+    {
+        int shapeCount = _mesh.blendShapeCount;
+        string[] blendShapeName = new string[shapeCount];
+        int[] blendShapeIndex = new int[shapeCount];
+        
+        if (Input.GetKeyDown(blendShapeOriginalTriggerKey)) {
+            for (int i = 0; i < shapeCount; i++) {
+                CubeSkinnedMeshRenderer.SetBlendShapeWeight(i, 0);
+            }
         }
     }
 }
