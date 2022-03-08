d6d45e56376808e86103772da5c79ec40a9c310d
Jing Tan <jtan1993@gmail.com>
2022-03-08 14:54:25 +0000

    add coroutine example

--- Assets/Scripts/BlendShapeSample.cs
+++ Assets/Scripts/BlendShapeSample.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Advanced BlendShape coroutine example: https://github.com/spiderlili/URP-2020-Shaders-Library/blob/main/Assets/Scripts/CrackController.cs
+
 public class BlendShapeSample : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer CubeSkinnedMeshRenderer;
@@ -9,6 +11,8 @@ public class BlendShapeSample : MonoBehaviour
     [SerializeField] private KeyCode blendShape0TriggerKey = KeyCode.A;
     [SerializeField] private KeyCode blendShape1TriggerKey = KeyCode.S;
     [SerializeField] private KeyCode blendShapeOriginalTriggerKey = KeyCode.D;
+    [SerializeField] private KeyCode blendShape1CoroutineTriggerKey = KeyCode.X;
+    public float CoroutineSpeed = 1.0f;
     
     void Start()
     {
@@ -20,31 +24,13 @@ public class BlendShapeSample : MonoBehaviour
     
     void Update()
     {
-        int shapeCount = _mesh.blendShapeCount;
-        string[] blendShapeName = new string[shapeCount];
-        int[] blendShapeIndex = new int[shapeCount];
-
-        for (int i = 0; i < shapeCount; i++) {
-            blendShapeName[i] = _mesh.GetBlendShapeName(i);
-            blendShapeIndex[i] = _mesh.GetBlendShapeIndex(_mesh.GetBlendShapeName(i));
-            Debug.Log(blendShapeName[i] + ": " + blendShapeIndex[i]);
-        }
-
-        if (Input.GetKeyDown(blendShape0TriggerKey)) {
-            CubeSkinnedMeshRenderer.SetBlendShapeWeight(0, 100);
-            if (CubeSkinnedMeshRenderer.GetBlendShapeWeight(1) != 0) {
-                CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 0);
-            }
-        }
-
-        if (Input.GetKeyDown(blendShape1TriggerKey)) {
-            CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 100);
-        }
-
+        DebugBlendShapes();
+        BlendShape0OnTrigger();
+        BlendShape1OnTrigger();
+        BlendShape01CoroutineOnTrigger();
         ResetBlendShapeToOriginalOnTrigger();
     }
     
-    // TODO: Create function to reset blendshape
     void ResetBlendShapeToOriginalOnTrigger()
     {
         int shapeCount = _mesh.blendShapeCount;
@@ -56,5 +42,61 @@ public class BlendShapeSample : MonoBehaviour
                 CubeSkinnedMeshRenderer.SetBlendShapeWeight(i, 0);
             }
         }
+    }
+
+    void DebugBlendShapes()
+    {
+        int shapeCount = _mesh.blendShapeCount;
+        string[] blendShapeName = new string[shapeCount];
+        int[] blendShapeIndex = new int[shapeCount];
+
+        for (int i = 0; i < shapeCount; i++) {
+            blendShapeName[i] = _mesh.GetBlendShapeName(i);
+            blendShapeIndex[i] = _mesh.GetBlendShapeIndex(_mesh.GetBlendShapeName(i));
+            Debug.Log(blendShapeName[i] + ": " + blendShapeIndex[i]);
+        }
+    }
+
+    void BlendShape0OnTrigger()
+    {
+        if (Input.GetKeyDown(blendShape0TriggerKey)) {
+            CubeSkinnedMeshRenderer.SetBlendShapeWeight(0, 100);
+            if (CubeSkinnedMeshRenderer.GetBlendShapeWeight(1) != 0) {
+                CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 0);
+            }
+        }
+    }
+    void BlendShape1OnTrigger()
+    {
+        if (Input.GetKeyDown(blendShape1TriggerKey)) {
+            CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 100);
+        }
+    }
+
+    
+    void BlendShape01CoroutineOnTrigger()
+    {
+        if (Input.GetKeyDown(blendShape1CoroutineTriggerKey)) {
+            StopAllCoroutines();
+            StartCoroutine(BlendShape01Coroutine());
+        }
+    }
+    
+    IEnumerator BlendShape01Coroutine()
+    {
+        float lerp = 0;
+        CubeSkinnedMeshRenderer.SetBlendShapeWeight(0, 0);
+        CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 0);
+        while (lerp < 1) {
+            CubeSkinnedMeshRenderer.SetBlendShapeWeight(1, 100 * lerp);
+            lerp += Time.deltaTime * CoroutineSpeed;
+            Debug.Log("Lerp: " + lerp);
+            yield return null;
+        }
+    }
+
+    public void SetBlendShape(int index, float value)
+    {
+        CubeSkinnedMeshRenderer.SetBlendShapeWeight(index, 100 - value); // Blend shape is open when the value is 0 & closed when the value is 100
     }
 }
